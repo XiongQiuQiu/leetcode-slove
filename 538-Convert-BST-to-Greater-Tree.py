77d191cfe64504af0b2c770ab25593ed029766fa
XiongQIuQIu <zhangjinwei94@163.com>
2017-07-05 17:59:49 +0800

    538

--- 538-Convert-BST-to-Greater-Tree.py
+++ 538-Convert-BST-to-Greater-Tree.py
@@ -1,9 +1,9 @@
-
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def convertBST(self, root):
@@ -22,17 +22,3 @@ class Solution(object):
 
         traverse(root)
         return root
-
-l = TreeNode(2)
-r = TreeNode(13)
-ro = TreeNode(5)
-ll = TreeNode(3)
-rr = TreeNode(4)
-ro.left = l
-ro.right = r
-l.left = ll
-l.right = rr
-l = [ro, l, rr, ll, rr]
-ans = Solution()
-print ans.convertBST(ro).val
-ans_nu = [x.val for x in l]
