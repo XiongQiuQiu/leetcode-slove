91c8aaf86a498fdfeddabda0bcd5deef242e4757
XiongQiuQiu <zhangjinwei94@163.com>
2017-11-22 18:36:30 +0800

    725 split linked list in parts

---
+++ 725-Split-Linked-List-in-Parts.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def countlen(self, root):
+        n = 0
+        while root:
+            n += 1
+            root = root.next
+        return n
+
+    def initans(self, l, k):
+        perlen = l / k
+        mod = l % k
+        ans = []
+        n = 0
+        for i in range(k):
+            if i < mod:
+                t = 1
+            else:
+                t = 0
+            ans.append(perlen + t)
+        return ans
+
+    def splitListToParts(self, root, k):
+        """
+        :type root: ListNode
+        :type k: int
+        :rtype: List[ListNode]
+        """
+        l = self.countlen(root)
+        num = self.initans(l, k)
+        ans = []
+        for one in num:
+            node = root
+            for i in range(one-1):
+                node = node.next
+            ans.append(root)
+            root = node.next
+            node.next = None
+        return ans
+
+
+
