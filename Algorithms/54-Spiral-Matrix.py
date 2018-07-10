a6c46fda40e564b15213e2fdc3e3d00175083b42
XiongQiuQiu <zjw2goo@gmail.com>
2018-07-10 18:37:47 +0800

    '2018/7/10'

---
+++ Algorithms/54-Spiral-Matrix.py
@@ -0,0 +1,36 @@
+'''
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+'''
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        ans = []
+        while matrix:
+            list_ = list(matrix.pop(0))
+            ans.extend(list_)
+            matrix = zip(*matrix)[::-1]
+        return ans
