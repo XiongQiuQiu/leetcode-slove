ba2408460da4d6266336e0307e68b6616c683004
XiongQiuQiu <zjw2goo@gmail.com>
2018-08-30 18:23:33 +0800

    '2018/8/30‘

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,7 +1,11 @@
-def generateMatrix(n):
-    A, lo = [], n*n+1
-    while lo > 1:
-        lo, hi = lo - len(A), lo
-        A = [range(lo, hi)] + zip(*A[::-1])
-    return A
-generateMatrix(3)
+def merge(nums1, m, nums2, n):
+    while m > 0 and n > 0:
+        if nums1[m - 1] >= nums2[n - 1]:
+            nums1[m + n - 1] = nums1[m - 1]
+            m -= 1
+        else:
+            nums1[m + n - 1] = nums2[n - 1]
+            n -= 1
+    if n > 0:
+        nums1[:n] = nums2[:n]
+merge( [1, 2, 3, 0, 0, 0],3,[2, 5, 6],3)
