8975e2415cfce1c41be8a5ba4eca7e4216e9c6b9
XiongQiuQiu <zjw2goo@gmail.com>
2018-09-12 18:50:02 +0800

    '2018/9/12'

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,15 +1,19 @@
-def isValid(s):
+def minimumDeleteSum(s1, s2):
     """
-    :type s: str
-    :rtype: bool
+    :type s1: str
+    :type s2: str
+    :rtype: int
     """
-    sd = {'(': ')', '[': ']', '{': '}'}
-    stack = []
-    for symbol in s:
-        if symbol in sd:
-            stack.append(symbol)
-        elif sd[stack.pop()] != symbol:
-            return False
-    return True
+    l1, l2 = len(s1), len(s2)
+    dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+    for j in range(1, l2 + 1):
+        dp[0][j] = dp[0][j - 1] + ord(s2[j - 1])
+    for i in range(1, l1 + 1):
+        dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
+        for j in range(1, l2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j] + ord(s1[i - 1]), dp[i][j - 1] + ord(s2[j - 1]))
 
-isValid('(]')
+    return dp[l1][l2]
