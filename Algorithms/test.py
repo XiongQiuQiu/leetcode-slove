65faee35ac997f1a0fc01583d1201e95070782f7
XiongQiuQiu <zjw2goo@gmail.com>
2018-12-03 15:25:51 +0800

    2018/12/3

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,23 +1,9 @@
-import collections
-def findLongestWord(s, d):
-    """
-    :type s: str
-    :type d: List[str]
-    :rtype: str
-    """
-    ans = []
-    dmap = collections.defaultdict(list)
-    for w in d:
-        dmap[w[0]].append((0, w))
-    for string in s:
-        wlist = dmap[string]
-        del dmap[string]
-        for i, w in wlist:
-            if i + 1 == len(w):
-                ans.append(w)
-            else:
-                dmap[w[i + 1]].append((i + 1, w))
-    if not ans: return ''
-    maxl = len(max(ans, key=len))
-    return min(w for w in ans if len(w) == maxl)
-findLongestWord( "abpcplea",["ale","apple","monkey","plea"])
+import tensorflow as tf
+with tf.device('/gpu:2'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print sess.run(c)
