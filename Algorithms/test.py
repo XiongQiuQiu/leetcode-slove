c3592565e20ea57a3155017c102dc53a441ef178
XiongQiuQiu <zjw2goo@gmail.com>
2019-01-03 15:55:23 +0800

    2019/01/03

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,19 +1,10 @@
-def combinationSum3(k, n):
-    """
-    :type k: int
-    :type n: int
-    :rtype: List[List[int]]
-    """
-    ans = []
-
-    def dfs(ans, stack, target, l,s):
-        for i in range(s, 11):
-            if target == 0 and l == 0:
-                ans.append(stack[:])
-                return
-            if target-i < 0 or l-1 < 0:
-                return
-            dfs(ans, stack+[i], target - i, l - 1,i+1)
-    dfs(ans, [], n, k,1)
-    return ans
-print combinationSum3(3,15)
+def subsetsWithDup( S):
+    res = [[]]
+    S.sort()
+    for i in range(len(S)):
+        if i == 0 or S[i] != S[i - 1]:
+            l = len(res)
+        for j in range(len(res) - l, len(res)):
+            res.append(res[j] + [S[i]])
+    return res
+print subsetsWithDup([1,2,2])
