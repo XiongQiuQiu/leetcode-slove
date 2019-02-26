8ce128c31cf979443fc370e379f0d28f55ae0f8e
XiongQiuQiu <zjw2goo@gmail.com>
2019-02-26 14:48:30 +0800

    2019/02/26

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,45 +1,17 @@
-
-def lengthOfLIS(nums):
+def wiggleMaxLength( nums):
     """
     :type nums: List[int]
     :rtype: int
     """
     size = len(nums)
     dp = [1] * size
+    status = [0] * size
     for x in range(size):
         for y in range(x):
-            if nums[x] > nums[y]:
-                dp[x] = max(dp[x],dp[y]+1)
+            if (status[y] == 1 and nums[x] > nums[y]) or (status[y] == -1 and nums[x] < nums[y]) or (status ==0 and nums[x] != nums[y]):
+                status[x] = [-1, 1][dp[x] > dp[y] + 1] * status[y]
+                dp[x] = max(dp[x], dp[y] + 1)
     return max(dp) if dp else 0
 
-lengthOfLIS([10,9,2,5,3,7,101,18])
-def lenght(nums):
-    size = len(nums)
-    dp = [1] * size
-    for x in range(size):
-        for y in range(x):
-            if nums[x] > nums[y]:
-                dp[x] = max(dp[x] ,dp[y]+1)
-    return max(dp) if dp else 0
 
-def lengthOfLIS1(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    size = len(nums)
-    dp = []
-    for x in range(size):
-        print
-        low, high = 0, len(dp) - 1
-        while low <= high:
-            mid = (low + high) / 2
-            if dp[mid] >= nums[x]:
-                high = mid - 1
-            else:
-                low = mid + 1
-        if low >= len(dp):
-            dp.append(nums[x])
-        else:
-            dp[low] = nums[x]
-    return len(dp)
+wiggleMaxLength([1,7,4,9,2,5])
