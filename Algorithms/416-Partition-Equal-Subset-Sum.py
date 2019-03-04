926c5f8c2972eb3d3dcdd10fcbbf9944711f7a84
XiongQiuQiu <zjw2goo@gmail.com>
2019-03-04 18:53:43 +0800

    2019/03/04

--- Algorithms/416-Partition-Equal-Subset-Sum.py
+++ Algorithms/416-Partition-Equal-Subset-Sum.py
@@ -34,3 +34,36 @@ class Solution(object):
             for num2 in nset.copy():
                 nset.add(num + num2)
         return sums / 2 in nset
+
+
+class Solution(object):
+    def canFindSum(self, nums, target, ind, n, d):
+        if target in d: return d[target]
+        if target == 0:
+            d[target] = True
+        else:
+            d[target] = False
+            if target > 0:
+                for i in xrange(ind, n):
+                    if self.canFindSum(nums, target - nums[i], i + 1, n, d):
+                        d[target] = True
+                        break
+        return d[target]
+
+    def canPartition(self, nums):
+        s = sum(nums)
+        if s % 2 != 0: return False
+        return self.canFindSum(nums, s / 2, 0, len(nums), {})
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+        dp = [True] + [False] * (total / 2)
+        for i in xrange(1, len(nums) + 1):
+            for j in reversed(xrange(1, total / 2 + 1)):
+                dp[j] = dp[j] or (j - nums[i - 1] >= 0 and dp[j - nums[i - 1]])
+        return dp[-1] and not total % 2
